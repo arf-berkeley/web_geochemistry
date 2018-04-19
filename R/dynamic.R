193f09588b8567230b35ec5b87e794e0c6dc7ded
Jesse Norris <jesse.norris95@yahoo.com>
2018-04-18 20:26:35 -0700

    'dynamic.R' (DynamicPlot) - General method completition (add/remove/get/index, not selection) and documentaton.

--- R/dynamic.R
+++ R/dynamic.R
@@ -1,4 +1,4 @@
-# library("dplyr")
+library("dplyr")
 ### Example of '%>%' usage: http://genomicsclass.github.io/book/pages/dplyr_tutorial.html#pipe-operator-
 
 ### Requires: 'config.R', but its already loaded by 'ui.R'
@@ -149,6 +149,7 @@ setGeneric(name="setx",
 setMethod(f="setx",
 	signature="DynamicData",
 	definition=function(self, x){
+		print(paste("Check here:", x))
 		self@x = x
 		return(self)
 	}
@@ -220,181 +221,251 @@ setMethod(f="addSelection",
 library("ggplot2")
 
 ###########################################################
-#' An S4 class for dynamically tracking the layers (point or path) in conjunction with a ggplot2 figure
+#' An S4 class for dynamically building and tracking the layers
+#' (point or path) of a ggplot2 object
 #'
-#' @slot point A list of path layer names (first in, last out)
-#' @slot path A list of point layer names (first in, last out)
-#' @slot layer A complete list of layer names (point and path, first in, first out) 
-#' @slot selection Current point selection
+#' @author Jesse Norris \email{jesse.norris@berkeley.edu}
+#'
+#' @slot plot Plot created by a ggplot2 object
+#' @slot point A list of path layer ids (first in, last out)
+#' @slot path A list of point layer ids (first in, last out)
+#' @slot layer A complete list of layer ids (point and path, first in, first out) 
+#' @slot selection Data.Frame of selected values for emphasis
 DynamicPlot = setClass("DynamicPlot",
 	slots = c(
+		plot="gg",
 		point="character",
 		path="character",
 		layer="character",
 		selection="data.frame"
-	),
-	prototype=list(
-		point=c(),
-		path=c(),
-		layer=c(),
-		selection=data.frame()
 	)
 )
+############################################################
 
-###########################################################
-#' Add layer to a DynamicPlot object
+############################################################
+#' Initialize a ggplot2 object
 #'
-#' @details This should only be called internally by addPoint() or addPath()
+#' Redefines the 'plot' slot with a new ggplot2 object.
+#'
+#' @note The ggplot2 object used to initialize the DynamicPlot object should
+#'	have no layers.
+#'
+#' @seealso \code{\link{addPoint}} for adding 'point' layers and \code{\link{addPath}}
+#'	for adding 'path' layers
 #'
 #' @param self DynamicPlot object
-#' @param id A unique id to define the layer (such as the source name)
+#' @param layer The initial plot layer as ggplot2 object
 #' @return Updated DynamicPlot object
-setGeneric(name="addLayer", function(self, id) {
+setGeneric(name="initializePlot", function(self, layer) {
+	standardGeneric("initializePlot")
+})
+
+#' @describeIn DynamicPlot Initialize a ggplot2 object
+setMethod(f="initializePlot", signature="DynamicPlot", function(self, layer) {
+	self@plot = layer
+	return(self)
+})
+
+############################################################
+#' Add a plot element to the ggplot2 object
+#'
+#' @note \code{\link{addLayer}} should only be called internally by 
+#'	\code{\link{addPoint}} or \code{\link{addPath}} as it does not 
+#'	manipulate the ggplot2 object, only the list tracking the layer ids.
+#'
+#' @name  Add-DynamicPlot
+#' @param self DynamicPlot object
+#' @param id id to define the layer (such as the source name)
+#' @param type Type of the layer ("point" or "path")
+#' @return Updated DynamicPlot object
+NULL
+############################################################
+
+#' @rdname Add-DynamicPlot
+setGeneric(name="addLayer", function(self, id, type) {
 	standardGeneric("addLayer")
 })
 
-#// #//' @rdname DynamicPlot-methods
-setMethod(f="addLayer", signature="DynamicPlot", function(self, id) {
-	self@layer = c(self@layer, id)
+setMethod(f="addLayer", signature="DynamicPlot", function(self, id, type) {
+	self@layer = c(self@layer, paste(id, type))
 	return(self)
 })
 
-# ##########################################################
-#' Remove layer from a DynamicPlot object
+#' @rdname Add-DynamicPlot
+setGeneric(name="addPoint", function(self, element, id) {
+	standardGeneric("addPoint")
+})
+
+#' @describeIn DynamicPlot Add 'point' layer
+setMethod(f="addPoint", signature="DynamicPlot", function(self, element, id){
+	### Add the new point element to the plot
+	self@plot = self@plot %+% element
+
+	### Append the layer id to the point and layer lists
+	self@point = c(self@point, id)
+	self = addLayer(self, id, "point")
+	return(self)
+})
+
+#' @rdname Add-DynamicPlot
+setGeneric(name="addPath", function(self, element, id) {
+	standardGeneric("addPath")
+})
+
+#' @describeIn DynamicPlot Add 'path' layer
+setMethod(f="addPath", signature="DynamicPlot", function(self, element, id){
+	### Add the new point element to the plot
+	self@plot = self@plot %+% element
+
+	self@path = c(self@path, id)
+	self = addLayer(self, id, "path")
+	return(self)
+})
+
+############################################################
+#' Remove a plot element from the ggplot2 object
 #'
-#' @details This should only be called internally by removePoint() or removePath()
+#' Removing the elements from the ggplot2 object is preformed with
+#'	the \pkg{ggedit} CRAN package:
+#'		\describe{
+#'			\item{}{\url{https://cran.r-project.org/web/packages/ggedit/}}	
+#'		}
 #'
+#' @note \code{\link{removeLayer}} should only be called internally by 
+#'	\code{\link{removePoint}} or \code{\link{removePath}} as it does not 
+#'	manipulate the ggplot2 object, only the list tracking the layer ids.
+#'
+#' @name  Remove-DynamicPlot
 #' @param self DynamicPlot object
-#' @param id A unique id to define the layer (such as the source name)
+#' @param id id of the layer to remove
+#' @param type Type of the layer ("point" or "path")
 #' @return Updated DynamicPlot object
-setGeneric(name="removeLayer", function(self, id) {
+NULL
+############################################################
+
+#' @rdname Remove-DynamicPlot
+setGeneric(name="removeLayer", function(self, id, type) {
 	standardGeneric("removeLayer")
 })
 
-#// #//' @rdname DynamicPlot-methods
-setMethod(f="removeLayer", signature="DynamicPlot", function(self, id) {
-	self@layer = self@layer[!(self@layer == id)]
+setMethod(f="removeLayer", signature="DynamicPlot", function(self, id, type) {
+	self@layer = self@layer[!(self@layer == paste(id, type))]
 	return(self)
 })
 
-# ##########################################################
-#' Get the layer id for a given index
+#' @rdname Remove-DynamicPlot
+setGeneric(name="removePoint", function(self, id) {
+	standardGeneric("removePoint")
+})
+
+#' @describeIn DynamicPlot Remove 'point' layer
+setMethod(f="removePoint", signature="DynamicPlot", function(self, id){
+	### Grab the point layer index to remove from the plot
+	index = getPointIndex(fig, id)
+	self@plot = self@plot %>% ggedit::remove_geom("point", index)
+
+	### Remove the layer id from the point and layer lists
+	self@point = self@point[!(self@point == id)]
+	self = removeLayer(self, id, "point")
+	return(self)
+})
+
+#' @rdname Remove-DynamicPlot
+setGeneric(name="removePath", function(self, id) {
+	standardGeneric("removePath")
+})
+
+#' @describeIn DynamicPlot Remove 'path' layer
+setMethod(f="removePath", signature="DynamicPlot", function(self, id){
+	### Grab the path layer index to remove from the plot
+	index = getPathIndex(fig, id)
+	self@plot = self@plot %>% ggedit::remove_geom("path", index)
+
+	### Remove the layer id from the path and layer lists
+	self@path = self@path[!(self@path == id)]
+	self = removeLayer(self, id, "path")
+	return(self)
+})
+
+############################################################
+#' Get the layer id of an index
+#' 
+#' @note This is used to get the `source' id following a "plotly_click" event.
 #'
-# ' @details This should only be called internally by removePoint() or removePath()
-#'
+#' @name getLayer-DynamicPlot
 #' @param self DynamicPlot object
 #' @param index Index of layer
 #' @return The layer id
+NULL
+############################################################
+
+#' @rdname getLayer-DynamicPlot
 setGeneric(name="getLayer", function(self, index) {
 	standardGeneric("getLayer")
 })
 
-#// #//' @rdname DynamicPlot-methods
+#' @describeIn DynamicPlot Get id for `layer' slot
 setMethod(f="getLayer", signature="DynamicPlot", function(self, index) {
 	return(self@layer[index+1])
 })
 
+############################################################
+#' Get the index of a layer id
+#'
+#' The index is from the `layer', `point', or `path' list of the
+#'	associated DynamicPlot slot.
+#'
+#' @note The `layer' slot appends " point" or " path" to its elements
+#'	to differentiate between layer types of the same id (see \code{\link{addLayer}}).
+#'
+#' @name  getIndex-DynamicPlot
+#' @param self DynamicPlot object
+#' @param id id of the layer
+#' @return Index of the layer id or 'NA'
+NULL
+############################################################
 
-setGeneric(name="addPoint",
-	def=function(self, id) {
-		standardGeneric("addPoint")
-	}
-)
+#' @rdname  getIndex-DynamicPlot
+setGeneric(name="getLayerIndex", function(self, id) {
+	standardGeneric("getLayerIndex")
+})
 
-setMethod(f="addPoint",
-	signature="DynamicPlot",
-	definition=function(self, id){
-		self@point = c(self@point, id)
-		self = addLayer(self, id)
-		return(self)
-	}
-)
+#' @describeIn  getIndex-DynamicPlot Index of 'layer' slot
+setMethod(f="getLayerIndex", signature="DynamicPlot", function(self, id) {
+	return(match(id, self@layer))
+})
 
-setGeneric(name="removePoint",
-	def=function(self, id) {
-		standardGeneric("removePoint")
-	}
-)
+#' @rdname  getIndex-DynamicPlot
+setGeneric(name="getPointIndex", function(self, id) {
+	standardGeneric("getPointIndex")
+})
 
-setMethod(f="removePoint",
-	signature="DynamicPlot",
-	definition=function(self, id){
-		# print(paste("From:", length(self@point)))
-		self@point = self@point[!(self@point == id)]
-		self = removeLayer(self, id)
-		# print(paste("To:", length(self@point)))
-		return(self)
-	}
-)
+#' @describeIn  getIndex-DynamicPlot Index of 'point' slot
+setMethod(f="getPointIndex", signature="DynamicPlot", function(self, id){
+	return(match(id, self@point))
+})
 
-setGeneric(name="getPointIndex",
-	def=function(self, id) {
-		standardGeneric("getPointIndex")
-	}
-)
+#' @rdname  getIndex-DynamicPlot
+setGeneric(name="getPathIndex", function(self, id) {
+	standardGeneric("getPathIndex")
+})
 
-setMethod(f="getPointIndex",
-	signature="DynamicPlot",
-	definition=function(self, id){
-		# print(glue::glue("Looking for {id}"))
-		index = match(id, self@point)
-		# prijnt(glue::glue("Found at index {index}"))
-		return(index)
-	}
-)
+#' @describeIn  getIndex-DynamicPlot Index of 'path' slot
+setMethod(f="getPathIndex", signature="DynamicPlot", function(self, id){
+	return(match(id, self@path))
+})
 
 
-setGeneric(name="addPath",
-	def=function(self, id) {
-		standardGeneric("addPath")
-	}
-)
-
-setMethod(f="addPath",
-	signature="DynamicPlot",
-	definition=function(self, id){
-		self@path = c(self@path, id)
-		self = addLayer(self, id)
-		return(self)
-	}
-)
-
-setGeneric(name="removePath",
-	def=function(self, id) {
-		standardGeneric("removePath")
-	}
-)
-
-setMethod(f="removePath",
-	signature="DynamicPlot",
-	definition=function(self, id){
-		self@path = self@path[!(self@path == id)]
-		self = removeLayer(self, id)
-		return(self)
-	}
-)
-
-setGeneric(name="getPathIndex",
-	def=function(self, id) {
-		standardGeneric("getPathIndex")
-	}
-)
-
-setMethod(f="getPathIndex",
-	signature="DynamicPlot",
-	definition=function(self, id){
-		return(match(id, self@path))
-	}
-)
-
-
-result = function(self) {
-	cat("Points:", paste(self@point, collapse=", "), "\n")
-	cat("Paths:", paste(self@path, collapse=", "), "\n")
-	cat("Layers:", paste(self@layer, collapse=", "), "\n\n")
-}
-
+############################################################
+# Test the add/remove tracking of DynamicPlot()
+############################################################
 # test = DynamicPlot()
+
+# result = function(self) {
+# 	cat("Points:", paste(self@point, collapse=", "), "\n")
+# 	cat("Paths:", paste(self@path, collapse=", "), "\n")
+# 	cat("Layers:", paste(self@layer, collapse=", "), "\n\n")
+# }
 
 # label = "point1"
 # print(glue::glue("addPoint('{label}')"))
@@ -426,7 +497,70 @@ result = function(self) {
 # test = removePath(test, label)
 # result(test)
 
+# print(getLayer(test, 0))
 # print(getLayer(test, 1))
-# print(getLayer(test, 2))
-# print(getLayer(test, 3)) ### Should be 'NA' since there are only 2 elements in the list
+# print(getLayer(test, 2)) ### Should be 'NA' since there are only 2 elements in the list
 
+# print(getPointIndex(test, "point2"))
+# print(getPointIndex(test, "point1")) ### Should be 'NA'
+
+# print(getPathIndex(test, "path2"))
+# print(getPathIndex(test, "path1")) ### Should be 'NA'
+############################################################
+
+
+############################################################
+# Test the plotting functionality of DynamicPlot()
+#	fig@plot$labels$colour = NULL
+############################################################
+# x = "Rb"
+# y = "Mn"
+# plot = ggplot(data, mapping=aes_string(x=x, y=y, color="Source.Name"))
+
+# print("Initializing empty ggplot2 plot")
+# fig = DynamicPlot()
+# fig = initializePlot(fig, plot)
+# print(fig@plot)
+# result(fig)
+
+# print("Adding 'Alca-1' points")
+# df = data %>% filter(Source.Name == "Alca-1")
+# layer = geom_point(data %>% filter(Source.Name == "Alca-1"),
+# 	mapping=aes_string(x=x, y=y, color="Source.Name"), size=3)
+# fig = addPoint(fig, layer, "Alca-1")
+# print(fig@plot)
+# result(fig)
+
+# print("Adding 'Alca-2' points")
+# layer = geom_point(data %>% filter(Source.Name == "Alca-2"),
+# 	mapping=aes_string(x=x, y=y, color="Source.Name"), size=3)
+# fig = addPoint(fig, layer, "Alca-2")
+# print(fig@plot)
+# result(fig)
+
+# print("Removing 'Alca-2' points")
+# fig = removePoint(fig, "Alca-2")
+# print(fig@plot)
+# result(fig)
+
+# print("Adding 'Alca-1' path")
+# df = data %>% filter(Source.Name == "Alca-1")
+# layer = stat_ellipse(data %>% filter(Source.Name == "Alca-1"),
+# 	mapping=aes_string(x=x, y=y, color="Source.Name"), size=1)
+# fig = addPath(fig, layer, "Alca-1")
+# print(fig@plot)
+# result(fig)
+
+# print("Adding 'Alca-2' path")
+# df = data %>% filter(Source.Name == "Alca-2")
+# layer = stat_ellipse(data %>% filter(Source.Name == "Alca-2"),
+# 	mapping=aes_string(x=x, y=y, color="Source.Name"), size=1)
+# fig = addPath(fig, layer, "Alca-2")
+# print(fig@plot)
+# result(fig)
+
+# print("Removing 'Alca-1' path")
+# fig = removePath(fig, "Alca-1")
+# print(fig@plot)
+# result(fig)
+############################################################
